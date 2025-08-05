dfb13c79ea4c82de0a5f6561bb4283bb9f936600
pfm-mvp <christiaan.vanrooijen@pfm-intelligence.com>
2025-08-05 12:23:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="Retail ROI Portal", layout="wide")
+
 st.title("🏬 Welkom bij de Retail ROI Calculator")
 
 st.markdown("""
@@ -8,4 +9,6 @@ Welkom in het dashboard. Gebruik de navigatie links om een ROI-scenario te simul
 
 - 📈 Conversieboost op zaterdagen
 - 👣 Meer bezoekers & conversie
+
+Selecteer een pagina in het menu aan de linkerzijde om te starten.
 """)
